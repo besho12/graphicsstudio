6efca6e01865a77c55fbd24de1771d545743aad3
Beshoy <beshoy.ecladuos@onexpand.co.uk>
2025-09-19 21:43:44 +0300

    adjusting the navbar on the landing page

--- resources/views/frontend/partials/main-menu.blade.php
+++ resources/views/frontend/partials/main-menu.blade.php
@@ -77,24 +77,5 @@
                 </form>
             </li>
         @endif
-        @if (allCurrencies()?->where('status', 'active')->count() > 1 && $setting?->is_shop)
-            <li class="select_item">
-                <form id="setCurrencyHeader" action="{{ route('set-currency') }}" method="get">
-                    <select class="select_js" name="currency">
-                        @forelse (allCurrencies()?->where('status', 'active') as $currency)
-                            <option value="{{ $currency->currency_code }}"
-                                {{ getSessionCurrency() == $currency->currency_code ? 'selected' : '' }}>
-                                {{ $currency->currency_name }}
-                            </option>
-                        @empty
-                            <option value="USD"
-                                {{ getSessionCurrency() == 'USD' ? 'selected' : '' }}>
-                                {{ __('USD') }}
-                            </option>
-                        @endforelse
-                    </select>
-                </form>
-            </li>
-        @endif
     </ul>
 </ul>
