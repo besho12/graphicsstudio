d8488b8668a65cc28d981523fe0322fa65f98ca3
Beshoy <beshoy.ecladuos@onexpand.co.uk>
2025-09-27 16:21:34 +0300

    disable the menu

--- app/Http/Controllers/Frontend/HomePageController.php
+++ app/Http/Controllers/Frontend/HomePageController.php
@@ -29,103 +29,100 @@ class HomePageController extends Controller {
     use GlobalMailTrait;
 
     function index(): View {
-        // $sectionSetting = SectionSetting::first();
+        $sectionSetting = SectionSetting::first();
 
-        // $theme_name = Session::has('demo_theme') ? Session::get('demo_theme') : DEFAULT_HOMEPAGE;
-        // $sections = Section::whereHas("home", function ($q) use ($theme_name) {
-        //     $q->where('slug', $theme_name);
-        // })->get();
+        $theme_name = Session::has('demo_theme') ? Session::get('demo_theme') : DEFAULT_HOMEPAGE;
+        $sections = Section::whereHas("home", function ($q) use ($theme_name) {
+            $q->where('slug', $theme_name);
+        })->get();
 
-        // $hero = $sections->where('name', 'hero_section')->first();
-        // $aboutSection = $sections->where('name', 'about_section')->first();
+        $hero = $sections->where('name', 'hero_section')->first();
+        $aboutSection = $sections->where('name', 'about_section')->first();
 
-        // $faqs = Faq::select('id')->with(['translation' => function ($query) {
-        //     $query->select('faq_id', 'question', 'answer');
-        // }])->active()->latest()->take(4)->get();
+        $faqs = Faq::select('id')->with(['translation' => function ($query) {
+            $query->select('faq_id', 'question', 'answer');
+        }])->active()->latest()->take(4)->get();
 
-        // $projects = Project::select('id', 'slug', 'image','project_date','service_id')->with([
-        //     'translation' => function ($query) {
-        //     $query->select('project_id', 'title', 'project_category');
-        // },
-        // 'service' => function ($query) {
-        //     $query->select('id','slug');
-        // },
-        // 'service.translation' => function ($query) {
-        //     $query->select('service_id','title');
-        // },
-        // ])->whereHas('service', function ($query) {
-        //     $query->active();
-        // })->active()->latest()->get();
+        $projects = Project::select('id', 'slug', 'image','project_date','service_id')->with([
+            'translation' => function ($query) {
+            $query->select('project_id', 'title', 'project_category');
+        },
+        'service' => function ($query) {
+            $query->select('id','slug');
+        },
+        'service.translation' => function ($query) {
+            $query->select('service_id','title');
+        },
+        ])->whereHas('service', function ($query) {
+            $query->active();
+        })->active()->latest()->get();
 
-        // $teams = OurTeam::select('name', 'slug', 'designation', 'image')->active()->latest()->take(4)->get();
+        $teams = OurTeam::select('name', 'slug', 'designation', 'image')->active()->latest()->take(4)->get();
 
-        // $testimonialSection = $sections->where('name', 'testimonial_section')->first();
-        // $testimonials = Testimonial::select('id')->with(['translation' => function ($query) {
-        //     $query->select('testimonial_id', 'name', 'designation', 'comment');
-        // }])->active()->latest()->take(4)->get();
+        $testimonialSection = $sections->where('name', 'testimonial_section')->first();
+        $testimonials = Testimonial::select('id')->with(['translation' => function ($query) {
+            $query->select('testimonial_id', 'name', 'designation', 'comment');
+        }])->active()->latest()->take(4)->get();
 
-        // $latest_blogs = Blog::select('id', 'blog_category_id', 'slug', 'image', 'created_at')->with([
-        //     'translation'          => function ($query) {
-        //         $query->select('blog_id', 'title');
-        //     },
-        //     'category'             => function ($query) {
-        //         $query->select('id', 'slug');
-        //     },
-        //     'category.translation' => function ($query) {
-        //         $query->select('blog_category_id', 'title');
-        //     },
-        // ])->whereHas('category', function ($query) {
-        //     $query->active();
-        // })->active()->homepage()->latest()->take(3)->get();
+        $latest_blogs = Blog::select('id', 'blog_category_id', 'slug', 'image', 'created_at')->with([
+            'translation'          => function ($query) {
+                $query->select('blog_id', 'title');
+            },
+            'category'             => function ($query) {
+                $query->select('id', 'slug');
+            },
+            'category.translation' => function ($query) {
+                $query->select('blog_category_id', 'title');
+            },
+        ])->whereHas('category', function ($query) {
+            $query->active();
+        })->active()->homepage()->latest()->take(3)->get();
 
-        // $bannerSection = $sections->where('name', 'banner_section')->first();
-        // $brands = Brand::select('name', 'image', 'url')->active()->take(8)->get();
+        $bannerSection = $sections->where('name', 'banner_section')->first();
+        $brands = Brand::select('name', 'image', 'url')->active()->take(8)->get();
 
-        // $awards = Award::select('id', 'url')->with([
-        //     'translation' => function ($query) {
-        //         $query->select('award_id', 'year', 'title', 'sub_title', 'tag');
-        //     },
-        // ])->active()->latest()->take(4)->get();
+        $awards = Award::select('id', 'url')->with([
+            'translation' => function ($query) {
+                $query->select('award_id', 'year', 'title', 'sub_title', 'tag');
+            },
+        ])->active()->latest()->take(4)->get();
 
-        // $services = Service::select('id', 'slug', 'icon')->with(['translation' => function ($query) {
-        //     $query->select('service_id', 'title', 'short_description', 'btn_text');
-        // }])->active()->latest()->take(4)->get();
+        $services = Service::select('id', 'slug', 'icon')->with(['translation' => function ($query) {
+            $query->select('service_id', 'title', 'short_description', 'btn_text');
+        }])->active()->latest()->take(4)->get();
 
-        // $servicefeatureSection = $sections->where('name', 'service_feature_section')->first();
+        $servicefeatureSection = $sections->where('name', 'service_feature_section')->first();
 
-        // $chooseUsSection = $sections->where('name', 'choose_us_section')->first();
+        $chooseUsSection = $sections->where('name', 'choose_us_section')->first();
 
-        // $counterSection = $sections->where('name', 'counter_section')->first();
+        $counterSection = $sections->where('name', 'counter_section')->first();
 
-        // $plans = SubscriptionPlan::select('id', 'plan_price','expiration_date','button_url')->with([
-        //     'translation'          => function ($query) {
-        //         $query->select( 'subscription_plan_id','plan_name','short_description','description','button_text');
-        //     },
-        // ])->active()->orderBy('serial')->take(3)->get();
+        $plans = SubscriptionPlan::select('id', 'plan_price','expiration_date','button_url')->with([
+            'translation'          => function ($query) {
+                $query->select( 'subscription_plan_id','plan_name','short_description','description','button_text');
+            },
+        ])->active()->orderBy('serial')->take(3)->get();
 
-        // //
-        // return view('frontend.home.' . $theme_name . '.index', compact(
-        //     'sectionSetting',
-        //     'hero',
-        //     'aboutSection',
-        //     'faqs',
-        //     'projects',
-        //     'teams',
-        //     'testimonialSection',
-        //     'testimonials',
-        //     'latest_blogs',
-        //     'bannerSection',
-        //     'brands',
-        //     'awards',
-        //     'services',
-        //     'servicefeatureSection',
-        //     'chooseUsSection',
-        //     'counterSection',
-        //     'plans',
-        // ));
-
-        dd('here');
-        $this->services();
+        //
+        return view('frontend.home.' . $theme_name . '.index', compact(
+            'sectionSetting',
+            'hero',
+            'aboutSection',
+            'faqs',
+            'projects',
+            'teams',
+            'testimonialSection',
+            'testimonials',
+            'latest_blogs',
+            'bannerSection',
+            'brands',
+            'awards',
+            'services',
+            'servicefeatureSection',
+            'chooseUsSection',
+            'counterSection',
+            'plans',
+        ));
     }
     function changeTheme(string $theme) {
         if (cache()->get('setting')?->show_all_homepage != 1) {
