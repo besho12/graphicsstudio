a61c5f8c83ffcd07162b62624859a0fe4eeff2c4
Ahmed Hamdy <ahmedhamdy5882178@gmail.com>
2025-10-01 03:39:07 +0300

    change font

--- resources/views/frontend/home/main/sections/hero-area.blade.php
+++ resources/views/frontend/home/main/sections/hero-area.blade.php
@@ -1,17 +1,16 @@
 <div class="hero-wrapper hero-1" id="hero">
+    <div class="hero-bg-image">
+        <img src="{{ asset($hero?->global_content?->image) }}" alt="Hero Background" class="hero-main-image">
+    </div>
     <div class="container">
-        <div class="hero-style1">
-            <div class="row">
-                <div class="col-lg-12">
+        <div class="row align-items-center min-vh-100">
+            <div class="col-lg-6">
+                <div class="hero-content">
                     <h1 class="hero-title wow img-custom-anim-left" data-wow-duration="1.5s" data-wow-delay="0.1s">{{$hero?->content?->title}}</h1>
-
-                    <h1 class="hero-title text-lg-end wow img-custom-anim-right" data-wow-duration="1.7s"
-                        data-wow-delay="0.1s">{{$hero?->content?->title_two}}</h1>
-                </div>
-                <div class="col-lg-6 offset-lg-6">
-                    <p class="hero-text wow img-custom-anim-right" data-wow-duration="1.5s" data-wow-delay="0.1s">{!! clean(processText($hero?->content?->sub_title)) !!}</p>
-                    <div class="btn-group fade_right">
-                        <a href="{{$hero?->global_content?->action_button_url}}" class="btn wow img-custom-anim-right">
+                    <h1 class="hero-title-two wow img-custom-anim-left" data-wow-duration="1.7s" data-wow-delay="0.2s">{{$hero?->content?->title_two}}</h1>
+                    <p class="hero-text wow img-custom-anim-left" data-wow-duration="1.5s" data-wow-delay="0.3s">{!! clean(processText($hero?->content?->sub_title)) !!}</p>
+                    <div class="btn-group fade_left">
+                        <a href="{{$hero?->global_content?->action_button_url}}" class="btn btn-primary hero-btn wow img-custom-anim-left">
                             <span class="link-effect text-uppercase">
                                 <span class="effect-1">{{$hero?->content?->action_button_text}}</span>
                                 <span class="effect-1">{{$hero?->content?->action_button_text}}</span>
@@ -19,10 +18,6 @@
                         </a>
                     </div>
                 </div>
-            </div>
-            <div class="hero-year-tag wow img-custom-anim-left">
-                <img src="{{ asset($hero?->global_content?->hero_year_image) }}" alt="{{$hero?->content?->hero_year_text}}">
-                <p>{{$hero?->content?->hero_year_text}}</p>
             </div>
         </div>
     </div>
--- resources/views/frontend/layouts/styles.blade.php
+++ resources/views/frontend/layouts/styles.blade.php
@@ -1,3 +1,8 @@
+<!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    
     <link rel="stylesheet" href="{{ asset('frontend/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('frontend/css/all.min.css') }}">
     <link rel="stylesheet" href="{{ asset('frontend/css/magnific-popup.min.css') }}">
